1abad26cad634bab010518f698045a8ce545a1db
Ana Ceprnja <aceprnja@iun.edu>
2021-08-02 19:13:59 -0600

    casting float

--- python_casting.py
+++ python_casting.py
@@ -1,3 +1,4 @@
+# integers
 x = int(2)
 y = int(3.8)
 z = int("9")
@@ -5,3 +6,15 @@ z = int("9")
 print(x)
 print(y)
 print(z)
+
+# floats
+a = float(2)
+b = float(2.2)
+c = float("3.3")
+
+print(a)
+print(b)
+print(c)
+
+
+# strings
