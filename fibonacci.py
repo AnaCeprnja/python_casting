a8ccbdae8f3bebebfedec1918fe39f09ac723bff
Ana Ceprnja <aceprnja@iun.edu>
2021-08-14 08:52:56 -0600

    error fixed

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,11 @@
 fib = int(input("up to how many terms"))
 
-m1, n2 = 0, 1
+n1, n2 = 0, 1
 count = 0
 
 # is valid?
 
-if nterms <= 0:
+if fib <= 0:
     print("please enter a positive integer")
 
 # one term?
